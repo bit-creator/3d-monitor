9968ef99e7d362c11b51e617b9ea3d3b74698cbd
bit-creator <lliepjiokandxojimc@gmail.com>
2019-12-04 22:28:14 +0200

    set

--- MonitorDemoApplication/uvcoordinate.h
+++ MonitorDemoApplication/uvcoordinate.h
@@ -6,6 +6,29 @@ class UVCoordinate
 {
 public:
     UVCoordinate();
+    UVCoordinate(const UVCoordinate& UV);
+    UVCoordinate(const UVCoordinate&& UV);
+
+    UVCoordinate& operator=(const UVCoordinate& UV);
+    UVCoordinate& operator=(const UVCoordinate&& UV);
+
+    ~UVCoordinate();
+
+    void setU(double& U);
+    void setV(double& V);
+    void setW(double& W);
+
+    void setKoordinate(double& U, double& V, double& z);
+
+    double getX() const;
+    double getY() const;
+    double getZ() const;
+
+
+private:
+    double _U;
+    double _V;
+    double _W;
 };
 
 #endif // UVCOORDINATE_H
