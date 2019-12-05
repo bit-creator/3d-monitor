184de34b2e3b2410f0f569efba264726b2f9a627
bit-creator <lliepjiokandxojimc@gmail.com>
2019-12-05 13:56:54 +0200

    bagFix

---
+++ MonitorDemoApplication/documentmanager.cpp
@@ -0,0 +1,81 @@
+#include "documentmanager.h"
+#include "modelstl.h"
+
+DocumentManager::DocumentManager()
+    : _active_document(nullptr)
+{
+
+}
+
+DocumentManager& DocumentManager::GetInstance()
+{
+    static DocumentManager instance;
+    return instance;
+}
+
+std::shared_ptr<Document> DocumentManager::OpenDocument(QString filename, Document::DocumentType type)
+{
+    std::shared_ptr<Document> document = CreateDocumentByType(filename, type);
+
+    document->Open();
+
+    _open_documents.insert(std::pair<QString,std::shared_ptr<Document>>(filename, document));
+
+    return document;
+}
+
+bool DocumentManager::SaveDocumentAs(std::shared_ptr<Document> document, QString filename)
+{
+    return document->SaveAs(filename);
+}
+
+bool DocumentManager::SaveDocument(std::shared_ptr<Document> document)
+{
+    return document->Save();
+}
+
+bool DocumentManager::CloseDocument(std::shared_ptr<Document> document)
+{
+    bool result = false;
+
+    auto iterator = _open_documents.find(document->GetName());
+    if(iterator != _open_documents.end())
+    {
+        _open_documents.erase(iterator);
+        result = true;
+    }
+
+    return result;
+}
+
+std::shared_ptr<Document> DocumentManager::GetActiveDocument()
+{
+    return _active_document;
+}
+
+bool DocumentManager::RenameDocument(std::shared_ptr<Document> document, QString new_filename)
+{
+    bool result = false;
+
+    auto iterator = _open_documents.find(document->GetName());
+    if(iterator != _open_documents.end())
+    {
+        _open_documents.erase(iterator);
+        _open_documents.insert(std::pair<QString,std::shared_ptr<Document>>(new_filename, document));
+        result = true;
+    }
+
+    return result;
+}
+
+std::shared_ptr<Document> DocumentManager::CreateDocumentByType(QString filename, Document::DocumentType type)
+{
+    std::shared_ptr<Document> result = nullptr;
+
+    if(type == Document::DocumentType::STL)
+    {
+        result = std::make_shared<ModelSTL>(filename);
+    }
+
+    return result;
+}
---
+++ MonitorDemoApplication/modelstl.h
@@ -0,0 +1,30 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include "document.h"
+#include "triangle.h"
+
+class ModelSTL : public Document
+{
+public:
+    ModelSTL(QString filename);
+    ModelSTL(const ModelSTL& model);
+    ModelSTL(const ModelSTL&& model);
+
+    ModelSTL& operator=(const ModelSTL& model);
+    ModelSTL& operator=(const ModelSTL&& model);
+
+    virtual ~ModelSTL() override;
+
+    virtual DocumentType GetType() const override;
+    virtual bool Open() override;
+    virtual bool Save() override;
+
+    friend std::ifstream& operator>>(std::ifstream& in, ModelSTL model);
+    friend std::ofstream& operator<<(std::ofstream& out, const ModelSTL& model);
+
+private:
+    QString _model_title;
+};
+
+#endif // MODEL_H
